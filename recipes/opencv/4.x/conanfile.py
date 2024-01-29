20cf8e837607edc011b2d83b26a72d61ea21e3ba
BRZAKALA Adam <adam.brzakala@hexagon.com>
2024-01-29 10:58:54 +0100

    Use opencv mechanism for ffmpeg

--- recipes/opencv/4.x/conanfile.py
+++ recipes/opencv/4.x/conanfile.py
@@ -383,10 +383,13 @@ class OpenCVConan(ConanFile):
             return ["eigen::eigen"] if self.options.with_eigen else []
 
         def ffmpeg():
-            components = []
-            if self.options.get_safe("with_ffmpeg"):
-                components = ["ffmpeg::avcodec", "ffmpeg::avformat", "ffmpeg::avutil", "ffmpeg::swscale"]
-            return components
+            if self.options.with_ffmpeg == False:
+                return []
+            if self.settings.os == "Windows":
+                return ["opencv_videoio_ffmpeg460_64"]
+            if self.settings.os == "Linux":
+                return ["avutil", "avcodec", "avformat", "swscale"]
+            return []
 
         def gtk():
             return ["gtk::gtk"] if self.options.get_safe("with_gtk") else []
@@ -569,12 +572,16 @@ class OpenCVConan(ConanFile):
                 "mandatory_options": ["imgproc"],
                 "requires": ["opencv_imgproc"] + opencv_calib3d() + ipp(),
             },
+            "videoio_ffmpeg460_64": {
+                "is_built": self.options.with_ffmpeg and self.settings.os == "Windows",
+            },
             "videoio": {
                 "is_built": self.options.videoio,
                 "mandatory_options": ["imgcodecs", "imgproc"],
-                "requires": ["opencv_imgcodecs", "opencv_imgproc"] + ffmpeg() + ipp(),
+                "requires": ["opencv_imgcodecs", "opencv_imgproc"] + ipp() + ffmpeg(),
                 "system_libs": [
                     (self.settings.os == "Android" and int(str(self.settings.os.api_level)) > 20, ["mediandk", "android", "log", "camera2ndk"]),
+                    (self.settings.os == "Linux" and self.options.with_ffmpeg, ffmpeg()),
                 ],
                 "frameworks": [
                     (is_apple_os(self), ["Accelerate", "AVFoundation", "CoreGraphics", "CoreMedia", "CoreVideo", "QuartzCore"]),
@@ -1139,9 +1146,6 @@ class OpenCVConan(ConanFile):
         if self.options.get_safe("with_quirc"):
             self.requires("quirc/1.2")
         # videoio module dependencies
-        if self.options.get_safe("with_ffmpeg"):
-            # opencv doesn't support ffmpeg >= 5.0.0 for the moment (until 4.5.5 at least)
-            self.requires("ffmpeg/4.4.4")
         # freetype module dependencies
         if self.options.freetype:
             self.requires("freetype/2.13.0")
@@ -1248,10 +1252,6 @@ class OpenCVConan(ConanFile):
         replace_in_file(self, os.path.join(self.source_folder, "modules", "imgcodecs", "CMakeLists.txt"), "${GDAL_LIBRARY}", "GDAL::GDAL")
         if Version(self.version) >= "4.8.0":
             replace_in_file(self, os.path.join(self.source_folder, "modules", "imgcodecs", "CMakeLists.txt"), "${AVIF_LIBRARY}", "avif")
-
-        ## Fix detection of ffmpeg
-        replace_in_file(self, os.path.join(self.source_folder, "modules", "videoio", "cmake", "detect_ffmpeg.cmake"),
-                        "FFMPEG_FOUND", "ffmpeg_FOUND")
 
         ## Robust handling of wayland
         if self.options.get_safe("with_wayland"):
@@ -1393,23 +1393,6 @@ class OpenCVConan(ConanFile):
         tc.variables["WITH_NVCUVID"] = False
 
         tc.variables["WITH_FFMPEG"] = self.options.get_safe("with_ffmpeg", False)
-        if self.options.get_safe("with_ffmpeg"):
-            tc.variables["OPENCV_FFMPEG_SKIP_BUILD_CHECK"] = True
-            tc.variables["OPENCV_FFMPEG_SKIP_DOWNLOAD"] = True
-            # opencv will not search for ffmpeg package, but for
-            # libavcodec;libavformat;libavutil;libswscale modules
-            tc.variables["OPENCV_FFMPEG_USE_FIND_PACKAGE"] = "ffmpeg"
-            tc.variables["OPENCV_INSTALL_FFMPEG_DOWNLOAD_SCRIPT"] = False
-            if Version(self.version) >= "4.7.0":
-                tc.variables["OPENCV_FFMPEG_ENABLE_LIBAVDEVICE"] = False
-            ffmpeg_libraries = []
-            for component in ["avcodec",  "avformat", "avutil", "swscale", "avresample"]:
-                if component == "avutil" or self.dependencies["ffmpeg"].options.get_safe(component):
-                    ffmpeg_libraries.append(f"ffmpeg::{component}")
-                    ffmpeg_component_version = self.dependencies["ffmpeg"].cpp_info.components[component].get_property("component_version")
-                    tc.variables[f"FFMPEG_lib{component}_VERSION"] = ffmpeg_component_version
-            tc.variables["FFMPEG_LIBRARIES"] = ";".join(ffmpeg_libraries)
-
         tc.variables["WITH_GSTREAMER"] = False
         tc.variables["WITH_HALIDE"] = False
         tc.variables["WITH_HPX"] = False
